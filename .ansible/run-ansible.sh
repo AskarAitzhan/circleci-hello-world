89df328309f3cf6c2899a2466e4d9dc7ac1c4f76
Askar Aitzhan <askar.aitzhan@gmail.com>
2021-04-22 00:56:18 -0700

    Add run-ansible script

---
+++ .ansible/run-ansible.sh
@@ -0,0 +1,5 @@
+cat "[all]" > inventory.txt && \
+aws ec2 describe-instances \
+   --query 'Reservations[*].Instances[*].PublicIpAddress' \
+   --output text >> inventory.txt && \
+ansible-playbook -i inventory.txt main.yml --private-key $1
