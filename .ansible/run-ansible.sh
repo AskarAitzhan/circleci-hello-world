179f81fe023af132bbc0271bcfd6dc437bed153b
Askar Aitzhan <askar.aitzhan@gmail.com>
2021-04-23 06:27:42 -0700

    Add main-remote.yml

--- .ansible/run-ansible.sh
+++ .ansible/run-ansible.sh
@@ -1,5 +1,5 @@
 echo "[all]" > inventory.txt && \
 aws ec2 describe-instances \
    --query 'Reservations[*].Instances[*].PublicIpAddress' \
-   --output text >> inventory.txt && \
-ansible-playbook -i inventory.txt main.yml --private-key $1
+   --output text >> inventory.txt # && \
+# ansible-playbook -i inventory.txt main.yml --private-key $1
